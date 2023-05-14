767496a104e3c3e8763f040928154bab39f82832
Dima2323-tech <123169116+PavlovWorkerAcc@users.noreply.github.com>
2023-05-14 18:12:04 +0300

    Ver4

--- src/Commponents/Reviews/Reviews.jsx
+++ src/Commponents/Reviews/Reviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import reviews from "../../img/Reviews_Photo.png"
+import reviews from "../../img/ReviewsPhoto.png"
 const Reviews = () => {
   return (
     <div className='Reviews_Container'>
